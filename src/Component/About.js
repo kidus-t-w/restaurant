26bb15544327ec033b7de12d181f51d0624ec394
kidus-t-w <kidusk69@gmail.com>
2024-05-02 21:09:27 +0300

    Finished About component

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,12 +1,15 @@
 import React from "react";
 import AboutBackground from "../Assets/about-background.png";
-import AboutBackgroundImage from "./Assets/about-background-image.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
 import { BsFillPlayCircleFill } from "react-icons/bs";
 
 const About = () => {
   return (
     <div className="about-section-container">
       <div className="about-background-image-container">
+        <img src={AboutBackground} alt="" />
+      </div>
+      <div className="about-section-image-container">
         <img src={AboutBackgroundImage} alt="" />
       </div>
       <div className="about-section-text-container">
