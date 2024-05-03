2131273b644b9ab85026321bc2357b0f7a786f14
kidus-t-w <kidusk69@gmail.com>
2024-05-03 12:26:06 +0300

    Finished Landing Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Home from './Component/Home';
+import About from './Component/About';
+import Work from './Component/Work';
+import Testimonial from './Component/Testimonial';
+import Contact from './Component/Contact';
+import Footer from './Component/Footer';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
